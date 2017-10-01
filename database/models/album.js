f022e09e4f4546f3bb6dec2a8e8a4e3d713917c9
Coffeeteer <tgrieshiemer@gmail.com>
2017-09-30 21:31:21 -0400

    tried my own version of the Schema and models of the Artists and Albums instructed in lecture 72

--- database/models/album.js
+++ database/models/album.js
@@ -1,2 +1,16 @@
 // Todo: create Album Schema
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = Schema({
+	title: String,
+	date: Date,
+	copiesSold: Number,
+	numberTracks: Number,
+	image: String,
+	revenue: Number
+});
+
+const Album = mongoose.model({ 'album', AlbumSchema});
+
+module.export = Album;
--- database/models/artist.js
+++ database/models/artist.js
@@ -1,2 +1,23 @@
 // Todo: Create Artist Model
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ArtistSchema =  Schema({
+	name: String,
+	age: Number,
+	yearsActive: Number,
+	image: String,
+	genre: String,
+	website: String,
+	netWorth: Number,
+	labelName: String,
+	retired: Boolean,
+	Albums: [{
+		type: Schema.Types.ObjectId,
+		ref: 'album'		
+	}]
+});
+
+const Artist = mongoose.model({ 'artist', ArtistSchema});
+
+module.exports = Artist;
